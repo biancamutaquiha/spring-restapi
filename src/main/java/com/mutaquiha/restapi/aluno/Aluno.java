4158801daa0e538673220d9515f5f2db2882ad4f
Olivio Mutaquiha <olivio@homer.com.br>
2020-01-22 21:50:59 -0300

    Implementado rota para obter todos os alunos.

---
+++ src/main/java/com/mutaquiha/restapi/aluno/Aluno.java
@@ -0,0 +1,44 @@
+package com.mutaquiha.restapi.aluno;
+
+public class Aluno {
+
+    private long id;
+    private int idade;
+    private String nomeCompleto;
+
+    public Aluno(long id, int idade, String nomeCompleto) {
+        this.id = id;
+        this.idade = idade;
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public Aluno(){
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+
+}
--- src/main/java/com/mutaquiha/restapi/aluno/AlunoController.java
+++ src/main/java/com/mutaquiha/restapi/aluno/AlunoController.java
@@ -3,11 +3,21 @@ package com.mutaquiha.restapi.aluno;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class AlunoController {
 
-    @RequestMapping("/")
-    public String hello(){
-        return "Hello World";
+    private static List<Aluno> alunos = new ArrayList<>();
+    static{
+        alunos.add(new Aluno(1, 10, "Olivio de Freitas"));
+        alunos.add(new Aluno(2, 13, "Bianca Ferreira"));
+        alunos.add(new Aluno(3, 11, "João Carvalho"));
+    }
+
+    @RequestMapping("/alunos")
+    public List<Aluno> getAll() {
+        return alunos;
     }
 }
